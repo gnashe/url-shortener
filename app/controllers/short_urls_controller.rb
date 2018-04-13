17937ce33077995512142469646d6a1635c1f08f
irogi <igorilic94@gmail.com>
2018-04-13 15:11:45 -0400

    Handle creation of new short_urls and perform original_url cleaning

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -11,8 +11,26 @@ class ShortUrlsController < ApplicationController
     @short_url = ShortUrl.find(params[:id])
   end
 
+  def create
+    @short_url = ShortUrl.new(short_url_params)
+    if @short_url.save
+      redirect_to @short_url
+      # handle successful save
+    else
+      # handle fault
+      render html: "uh oh! something went wrong"
+    end
+  end
+
   def redirect
     @short_url = ShortUrl.find_by_short_url(params[:short_url])
     redirect_to @short_url.original_url
   end
+
+  private
+
+    def short_url_params
+      params.require(:short_url).permit(:original_url)
+    end
+
 end
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -3,4 +3,25 @@ class ShortUrl < ApplicationRecord
   validates :original_url, presence: true,
                            uniqueness: { case_sensitive: false },
                            url: true
+
+  before_validation :clean_url, :create_short_url
+
+  REGEX_PROTOCOL = /https?:\/\//ix
+  PERMISSIBLE_VALS = [*'a'..'z', *'A'..'Z', *'0'..'9'].join
+
+  # ensure the url starts with it protocol and is normalized
+  def clean_url
+    url = self.original_url
+    url = url.strip
+    if url !~ REGEX_PROTOCOL
+      url = "http://#{url}"
+    end
+    self.original_url = URI.parse(url).normalize.to_s
+  end
+
+  def create_short_url
+    path = (0...6).map { PERMISSIBLE_VALS[rand(PERMISSIBLE_VALS.size)] }.join
+    self.short_url = path
+  end
+
 end
